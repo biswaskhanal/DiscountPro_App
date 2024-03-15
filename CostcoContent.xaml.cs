087d582f91910ae9f9f58d16367dd9bc091c7512
Afzal Vahora <afzalvhora1234@gmail.com>
2024-03-15 09:06:49 -0400

    add coments for CostcoContent.xaml.cs

--- CostcoContent.xaml.cs
+++ CostcoContent.xaml.cs
@@ -2,10 +2,13 @@
 
 namespace MauiApp2
 {
+    // A content page for displaying Costco content is represented by this class.
     public partial class CostcoContent : ContentPage
     {
+        //Constructor to set up a fresh instance
         public CostcoContent()
         {
+            // To initialise the XAML-defined UI components, call the InitializeComponent method.
             InitializeComponent();
         }
     }
