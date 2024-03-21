9daa327b408b7d1f3f4a0fc71a68fcf02baaadba
biswas <biswas>
2024-03-20 21:38:30 -0400

    Added frame for each image to make screen attractive

--- MauiApp2/WalmartContent.xaml.cs
+++ MauiApp2/WalmartContent.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.Maui.Controls;
+using System;
 
 namespace MauiApp2
 {
-    // A content page for displaying Walmart content is represented by this class.
     public partial class WalmartContent : ContentPage
     {
-        //Constructor to set up a fresh instance
         public WalmartContent()
         {
-            // To initialise the XAML-defined UI components, call the InitializeComponent method.
             InitializeComponent();
         }
+
     }
 }
